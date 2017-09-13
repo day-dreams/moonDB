b061399d01a0c39f5bc777273d81f2b988b6dc19
daydream <zn.moon2016@gmail.com>
2017-09-13 19:32:06 +0800

    添加工厂函数

--- db/db.go
+++ db/db.go
@@ -7,29 +7,38 @@ Package db 实现最基本的数据库引擎，负责具体的数据操作。
 
 package db
 
+import "fmt"
+
 type DbBase struct {
 	storage map[string]string
 }
 
-func (db *DbBase) set(key, value string) {
+func GenerateDbBase() DbBase {
+	return DbBase{storage: make(map[string]string)}
+}
+
+func (db *DbBase) Set(key, value string) {
 	db.storage[key] = value
 }
 
-func (db *DbBase) get(key string) (string, error) {
+func (db *DbBase) Get(key string) (string, error) {
 	ret := db.storage[key]
 	if ret == "" {
 		return "", DbError{"no such key:" + key}
 	}
+
 	return ret, nil
 }
 
-func (db *DbBase) existed(key string) bool {
+func (db *DbBase) Existed(key string) bool {
 	if db.storage[key] == "" {
 		return false
 	}
 	return true
 }
 
-func (db *DbBase) del(key string) {
+func (db *DbBase) Del(key string) {
 	delete(db.storage, key)
+	fmt.Println(db.storage)
+
 }
