2942dbf4bcb30b2baa368c3ce463ef0342fb5f3c
daydream <zn.moon2016@gmail.com>
2017-10-25 10:29:26 +0800

    分离logging的声明实现

--- include/moonDB/logging.h
+++ include/moonDB/logging.h
@@ -11,8 +11,8 @@
 #include <string>
 using namespace std;
 
-namespace logging
-{
+namespace moon {
+namespace logging {
 const int DEBUG = 1;
 const int INFO = 2;
 const int ERROR = 3;
@@ -23,66 +23,19 @@ const int FATAL = 4;
 
 #define set_print(need_print) _detail::_set_print(need_print);
 
-namespace _detail
-{
-
-mutex lock;
-bool need_print = false;
-fstream log_file;
+namespace _detail {
 
 /*
     get_time_now 获取时间字符串并返回
 */
-string get_time_now()
-{
-    auto now = chrono::system_clock::to_time_t(chrono::system_clock::now());
-    stringstream buffer;
-    buffer << put_time(localtime(&now), "%F %T");
-    return buffer.str();
-}
+string get_time_now();
 
-void _set_print(bool flag)
-{
-    lock.lock();
-    need_print = flag;
-    lock.unlock();
-}
+void _set_print(bool flag);
 
 /*
     log 将日志信息打印到文件中
 */
-void _log(string detail, string filename, int loglevel, int linenum)
-{
-    lock.lock();
-
-    string loglevel_str;
-    switch (loglevel)
-    {
-    case DEBUG:
-        loglevel_str = "DEBUG";
-        break;
-    case INFO:
-        loglevel_str = "INFO ";
-        break;
-    case ERROR:
-        loglevel_str = "ERROR";
-        break;
-    case FATAL:
-        loglevel_str = "FATAL";
-        break;
-    default:
-        loglevel_str = "INFO ";
-    }
-    if (!log_file.is_open())
-        log_file.open("log.txt", fstream::app);
-    log_file << get_time_now() << " " << filename << "," << linenum << " "
-             << loglevel_str << " : " << detail << endl;
-    if (need_print)
-    {
-        cout << get_time_now() << " " << filename << "," << linenum << " "
-             << loglevel_str << " : " << detail << endl;
-    }
-    lock.unlock();
+void _log(string detail, string filename, int loglevel, int linenum);
 }
 }
 }
