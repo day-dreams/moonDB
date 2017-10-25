f9a52e830c31cf0a9ff064558a2e096a6059e1bc
daydream <zn.moon2016@gmail.com>
2017-10-25 11:59:16 +0800

    模板类实现声明不可分离！

--- include/moonDB/LockFreeQueue.h
+++ include/moonDB/LockFreeQueue.h
@@ -5,6 +5,7 @@
 #include <mutex>
 #include <queue>
 #include <semaphore.h>
+using std::make_shared;
 using std::queue;
 using std::mutex;
 using std::shared_ptr;
@@ -22,28 +23,77 @@ private:
   int maxnum;
 
 public:
-  LockFreeQueue();
-  LockFreeQueue(int maxnum);
+  LockFreeQueue() {
+    sem_init(&client_arrived, 0, 0);
+    maxnum = 30;
+  }
+  LockFreeQueue(int maxnum) {
+    sem_init(&client_arrived, 0, 0);
+    this->maxnum = maxnum;
+  }
 
   /*
       push 向队列中加入一个元素
   */
-  void push(const T &ele);
+  void push(const T &ele) {
+
+    full.lock(); // if queue is full,this will block until pop() unlcok it
+
+    lock.lock();
+    _queue.push(ele);
+    if (_queue.size() == maxnum) { // queue is full,so hold full mutex
+    } else {                       // or,just unlock it
+      full.unlock();
+    }
+    lock.unlock();
+
+    sem_post(&client_arrived);
+  }
 
   /*
     pop 从队列中取出一个元素,如果队列为空则陷入等待
   */
-  T pop();
+  T pop() {
+    sem_wait(&client_arrived);
+
+    lock.lock();
+
+    if (_queue.size() == maxnum) // queue is not full anymore,so unlock it
+      full.unlock();
+    auto result = _queue.front();
+    _queue.pop();
+
+    lock.unlock();
+
+    return result;
+  }
 
   /*
-    pop 尝试从队列中取出一个元素,如果队列为空则立即返回
+    try_pop 尝试从队列中取出一个元素,如果队列为空则立即返回
   */
-  shared_ptr<T> try_pop();
+  shared_ptr<T> try_pop() {
+    auto rvalue = sem_trywait(&client_arrived);
+    if (rvalue == 0) {
+      lock.lock();
+      if (_queue.size() == maxnum) // queue is not full anymore,so unlock it
+        full.unlock();
+      auto result = make_shared<T>(_queue.front());
+      _queue.pop();
+      return result;
+    } else {
+      return shared_ptr<T>();
+    }
+  }
 
   /*
     size 返回队列元素个数
   */
-  size_t size();
+  size_t size() {
+    lock.lock();
+    auto size = _queue.size();
+    lock.unlock();
+    return size;
+  }
 };
 }
 #endif
